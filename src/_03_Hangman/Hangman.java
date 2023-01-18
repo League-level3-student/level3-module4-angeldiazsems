4ecaafc80c7321baf0354f002a681acf334c904a
student <student@ip-172-31-62-216.us-west-2.compute.internal>
2023-01-17 22:01:39 -0800

    l

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -10,25 +10,30 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Hangman implements KeyListener {
 	Stack<String> words = new Stack<String>();
 	Stack<Character> stack = new Stack<Character>();
 	StringBuilder bob = new StringBuilder("");
 	StringBuilder bobby;
+	int lifes = 5;
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel("Hangman");
+	JLabel label2 = new JLabel("| LIVES LEFT : " + lifes + " |");
 	String letters;
 	String hiddenWord;
 	String currentWord;
 
 	void setup() {
-
+		lifes = 5;
 		frame.setVisible(true);
 		frame.setSize(300, 300);
+		label2.setBounds(200, 200, 200, 200);
 		frame.add(panel);
 		panel.add(label);
+		panel.add(label2);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addKeyListener(this);
 
@@ -47,13 +52,13 @@ public class Hangman implements KeyListener {
 				words.push(word);
 				System.out.println(word);
 			} else {
-				
+
 			}
 		}
 
 	}
 
-	void code() {
+	void code() {  //pops word into game and formats it
 
 		System.out.println(words.size());
 		currentWord = words.pop();
@@ -67,7 +72,7 @@ public class Hangman implements KeyListener {
 		bobby = new StringBuilder(letters);
 		String noLetters = bobby.toString().replaceAll(regex, "*"); // replacing letters with *
 		bobby = new StringBuilder(noLetters);
-		String noSpace  = bobby.toString().replaceAll(regex3, "");
+		String noSpace = bobby.toString().replaceAll(regex3, "");
 		bobby = new StringBuilder(noSpace);
 		hiddenWord = bobby.toString().replaceAll(regex2, "");
 		label.setText(hiddenWord);
@@ -76,32 +81,49 @@ public class Hangman implements KeyListener {
 
 	}
 
+	void gameOver() {
+		String answer = JOptionPane.showInputDialog("You lost all your lives. Do you want to play again(y or n)");
+		if(answer.equals("y") || answer.equals("yes")) {
+			setup();
+			code();
+		}
+		else {
+			System.exit(0);
+		}
+	} 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stun
 		ArrayList<Integer> index = new ArrayList<Integer>();
-		String keyPressed = ""+arg0.getKeyChar();
+		String keyPressed = "" + arg0.getKeyChar();
+
 		
 		for (int i = 0; i < currentWord.length(); i++) {
 			if (arg0.getKeyChar() == currentWord.charAt(i)) {
 				index.add(i);
 			}
 		}
-		System.out.println(""+arg0.getKeyChar()+" is at index : "+index);
-		
-		for(int i =0; i < index.size(); i++) {
-			bobby = new StringBuilder(hiddenWord);
-			int index2 = index.get(i);			
-			hiddenWord = bobby.replace(index2, index2+1, keyPressed).toString();
-			System.out.println("Replacing");
-			label.setText(hiddenWord);
+
+		if (index.size() != 0) {
+			for (int i = 0; i < index.size(); i++) {
+				bobby = new StringBuilder(hiddenWord);
+				int index2 = index.get(i);
+				hiddenWord = bobby.replace(index2, index2 + 1, keyPressed).toString();
+				System.out.println("Replacing");
+				label.setText(hiddenWord);
+			}
+		}
+		else {
+			lifes--;
+			System.out.println(""+keyPressed+" is not part of the word");
+			label2.setText("| LIVES LEFT : " + lifes + " |");
+			if(lifes <= 0) {
+				gameOver();
+			}
 		}
 		
-	
-	
-	
-	
-	
+		
+		
 	}
 
 	@Override
